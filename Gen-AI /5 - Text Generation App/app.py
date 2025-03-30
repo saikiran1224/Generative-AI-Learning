076419c6b3c74b7a7c924fa3f99c314e83b08bd9
saikiran1224 <knvrssaikiran@gmail.com>
2025-03-30 22:00:35 +0530

    Fix: Error fixed and completed Text Generation App

--- Gen-AI /5 - Text Generation App/app.py
+++ Gen-AI /5 - Text Generation App/app.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv # importing dotenv to load environment variables
 import os 
-from openai import OpenAI # importing openai to use the GPT-3 API
+from openai import AzureOpenAI # importing openai to use the GPT-3 API
 
 # LEARNING 0 - Difference between accessing environment variables using dotenv and os
 # "dotenv" is a package that loads environment variables from a .env file into os.environ without need of 
@@ -9,19 +9,34 @@ from openai import OpenAI # importing openai to use the GPT-3 API
 
 load_dotenv() # loading environment variables from our .evn file 
 
-
 # Connecting to the Azure OpenAI API
-client = OpenAI()
+client = AzureOpenAI(  
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),  
+    api_key=os.getenv("OPENAI_API_KEY"),  
+    api_version="2024-05-01-preview",
+)
+
+# Provining the chat prompt
+chat_prompt = [
+   {
+      "role": "user",
+      "content": [
+          {
+            "type": "text",
+            "text": "Write a scene set in ancient Rome, focusing on the daily life of a common citizen."
+          }
+        ]
+    }
+]
 
 # Defining the function that will generate text
 completion = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {"role": "developer", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ]
+  model=os.getenv("DEPLOYMENT_NAME"), # the model to use for completion
+  messages=chat_prompt, # the prompt to complete
+  max_tokens=800, # maximum number of tokens to generate
+  temperature=1.0, # 0.0 is deterministic, 1.0 is maximum randomness
 )
 
-print(completion.choices[0].message)
+print(completion.to_json()) # printing the generated text
 
 
